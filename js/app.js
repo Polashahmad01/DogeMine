20ccf64f926d3593ffe3ba0388e62e200f66e99b
Istiak Ahmad Polash <dev.polashahmad@gmail.com>
2020-06-07 11:34:53 +0600

    Mobile Version Complete

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// Smartphone Menu Styling
+const toggler = document.getElementById('hamburger');
+
+// Listen to event
+toggler.addEventListener('click', loadMenu);
+
+// Load Menu Function
+function loadMenu() {
+  
+}
